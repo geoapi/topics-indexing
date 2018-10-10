ec17372b7e598c68389789c7e76da406c06499f1
George <geo@itnet.uobabylon.edu.iq>
2018-10-10 20:28:38 +1100

    Update indexing.py
    added indexing bag of keywords

--- indexing.py
+++ indexing.py
@@ -21,11 +21,11 @@ def create_index(topics_content,topics_index):
                topics_index[t].append(j['_id'])
     return (topics_index)
 
-def create_we_index(topics_content,topics_index):
+#TODO here we need to search if a keyword exists as is and then we need to add duplicates for keywords based on the main topic name e.g. safty takes posts that has safty keyword and all security posts as well
+def create_bok_index(topics_content,topics_index):
     for i in topics_content:
-       for j in i['wiki']:
+      for j in i['keywords']:
         t = j
-        #TODO sanitize the term t for example remove any backslashes from it / \ - or special chars
         #print(t); #prints all topics names from the json document
         url = "http://localhost:9200/question/so/_search?"
         params = dict(
@@ -33,7 +33,29 @@ def create_we_index(topics_content,topics_index):
         )
         resp = requests.get(url=url, params=params)
         data = resp.json()
-        if ( resp.status_code !==400):
+        data2 = data['hits']['hits']
+        dic = []
+        if ((len(data2)) > 0):
+           #k=0
+           for j in data2:
+              # dic.insert(k,j['_id'])
+              if (j['_id'] not in topics_index[t]):
+               topics_index[t].append(j['_id'])
+    return (topics_index)
+
+def create_we_index(topics_content,topics_index):
+    for i in topics_content:
+       for j in i['wiki']:
+        t = j
+        #TODO sanitize the term t for example remove any backslashes from it / \ - or special chars use Elasticsearch analyzers or nlp for this
+        #print(t); #prints all topics names from the json document
+        url = "http://localhost:9200/question/so/_search?"
+        params = dict(
+            q = t
+        )
+        resp = requests.get(url=url, params=params)
+        data = resp.json()
+        if ( resp.status_code != 400):
             data2 = data['hits']['hits']
             dic = []
             if ((len(data2)) > 0):
@@ -41,6 +63,8 @@ def create_we_index(topics_content,topics_index):
                for j in data2:
                   # dic.insert(k,j['_id'])
                    topics_index[t].append(j['_id'])
+        else:
+            print("resp status code is ",resp.status_code )
 
     return (topics_index)
 
@@ -51,9 +75,18 @@ def init_simple_keyword_index():
     topics_content = doc['topics']
     topics_index = create_index(topics_content,topics_index)
     print(topics_index)
-    print(topics_index['oauth configuration'])
+    print(topics_index['security'])
     return (topics_index)
 
+def init_bag_of_keywords_index():
+    with open('./topics.json','r') as f:
+        doc = json.load(f)
+    topics_index = defaultdict(list)
+    topics_content = doc['topics']
+    topics_index = create_bok_index(topics_content,topics_index)
+    print(topics_index)
+    print(topics_index['security'])
+    return (topics_index)
 
 def init_we_keyword_index():
     with open('./valid-topics.json','r') as f:
@@ -62,9 +95,14 @@ def init_we_keyword_index():
     topics_content = doc['topics']
     topics_index = create_we_index(topics_content,topics_index)
     print(topics_index)
-    print(topics_index['oauth configuration'])
+    print(topics_index['security'])
     return (topics_index)
 
 #indexing only API Topics keywords
 #init_simple_keyword_index()
-init_we_keyword_index()
+
+#indexing using bag of keywords
+init_bag_of_keywords_index()
+
+#This indexing is using Wiki WE indexing
+#init_we_keyword_index()
